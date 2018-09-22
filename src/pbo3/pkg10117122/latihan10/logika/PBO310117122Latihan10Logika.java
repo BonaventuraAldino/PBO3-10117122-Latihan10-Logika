1c5e2de928822090732c4d2792dd90ad9b3551c4
BonaventuraAldino <aldysenda99@gmail.com>
2018-09-23 01:24:23 +0700

    PBO3-10117122-Latihan10-Logika

---
+++ src/pbo3/pkg10117122/latihan10/logika/PBO310117122Latihan10Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan10.logika;
+
+/**
+ *
+ * @author lenovo
+ * 
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122 
+ * 
+ */
+public class PBO310117122Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
